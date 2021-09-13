8b2d368856d77f296b6330515a86861354055c4c
Charon416 <1062207473@qq.com>
2021-09-13 19:42:32 +0800

    aa

---
+++ src/views/category/childComps/CateGoryNavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <nav-bar>
+      <div slot="center">
+        {{title}}
+      </div>
+
+    </nav-bar>
+
+  </div>
+  
+</template>
+
+<script>
+
+import NavBar from "components/common/navbar/NavBar"
+  export default {
+    name:'CateGoryNavBar',
+    components:{
+      NavBar
+    },
+    data(){
+      return{
+        title:'商品分类'
+      }
+    }
+
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/views/category/childComps/TabMenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="menu-list">
+    <div class="menu-list-item"
+          :class="{active: index===currentIndex}"
+          v-for="(item, index) in categories"
+          :key="index"
+          @click="itemClick(index)">
+      {{item.title}}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'TabMenu',
+    data(){
+
+    },
+    props:{
+      categories:{
+        type:Array,
+        default(){
+          return []
+        }
+      }
+        
+    }
+  }
+
+  
+</script>
+
+<style>
+
+</style>
